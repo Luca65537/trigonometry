be7b8d2d4d08a5353edd02032bbeada353d56067
Luca65537 <107449500+Luca65537@users.noreply.github.com>
2024-02-15 00:57:01 -0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,14 @@
+def calculate_angles(x: number, y: number, l1: number, l2: number):
+    global a, b, theta1, theta2
+    a = (x ** 2 + y ** 2 - (l1 ** 2 - l2 ** 2)) / (2 * (l1 * l2))
+    b = Math.sqrt(x ** 2 + y ** 2)
+    theta1 = 0
+    theta2 = 0
+theta2 = 0
+theta1 = 0
+b = 0
+a = 0
+length1 = 40
+length2 = 30
+targetX = 50
+targetY = 40
---
+++ main.ts
@@ -0,0 +1,16 @@
+function calculate_angles(x: number, y: number, l1: number, l2: number) {
+    
+    a = (x ** 2 + y ** 2 - (l1 ** 2 - l2 ** 2)) / (2 * (l1 * l2))
+    b = Math.sqrt(x ** 2 + y ** 2)
+    theta1 = 0
+    theta2 = 0
+}
+
+let theta2 = 0
+let theta1 = 0
+let b = 0
+let a = 0
+let length1 = 40
+let length2 = 30
+let targetX = 50
+let targetY = 40
