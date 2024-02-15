2d286331dcd9b782d48691433313524bc58c2f31
Luca65537 <107449500+Luca65537@users.noreply.github.com>
2024-02-15 01:00:07 -0300

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def calculate_angles(x: number, y: number, l1: number, l2: number):
-    global a, b, theta1, theta2
+    global a, b, theta2, theta1
     a = (x ** 2 + y ** 2 - (l1 ** 2 - l2 ** 2)) / (2 * (l1 * l2))
     b = Math.sqrt(x ** 2 + y ** 2)
-    theta1 = 0
-    theta2 = 0
-theta2 = 0
+    theta2 = Math.acos(max(min(a, 1), -1))
+    # Ensure 'a' is within the valid range for acos
+    theta1 = Math.atan2(y, x) - Math.atan2(l2 * Math.sin(theta2), l1 + l2 * Math.cos(theta2))
 theta1 = 0
+theta2 = 0
 b = 0
 a = 0
 length1 = 40
--- main.ts
+++ main.ts
@@ -2,12 +2,13 @@ function calculate_angles(x: number, y: number, l1: number, l2: number) {
     
     a = (x ** 2 + y ** 2 - (l1 ** 2 - l2 ** 2)) / (2 * (l1 * l2))
     b = Math.sqrt(x ** 2 + y ** 2)
-    theta1 = 0
-    theta2 = 0
+    theta2 = Math.acos(Math.max(Math.min(a, 1), -1))
+    //  Ensure 'a' is within the valid range for acos
+    theta1 = Math.atan2(y, x) - Math.atan2(l2 * Math.sin(theta2), l1 + l2 * Math.cos(theta2))
 }
 
-let theta2 = 0
 let theta1 = 0
+let theta2 = 0
 let b = 0
 let a = 0
 let length1 = 40
